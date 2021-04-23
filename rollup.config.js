87cd4b68c8849951811f12df55ee94ddcb4aab1e
meckenbach <mle9@student.london.ac.uk>
2021-04-23 07:50:00 +0200

    style: standardjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,31 @@
-"use strict";
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
 
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-
-import pkg from "./package.json"
+import pkg from './package.json'
 
 export default {
-    input: "src/main.js",
-    output: {
-        file: `public/${pkg.name}.js`,
-        format: "iife",
-        globals: {
-            "react": "React",
-            "react-dom": "ReactDOM"
-        }
-    },
-    plugins: [
-        babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
-        // We need to convert the CommonJS modules used by Node to ES6
-        commonjs(),
-        // Resolve Node modules
-        nodeResolve(),
-        replace({
-            "process.env.NODE_ENV": JSON.stringify("production")
-        })
-    ],
-    watch: {
-        exclude: ['node_modules/**']
+  input: 'src/main.js',
+  output: {
+    file: `public/${pkg.name}.js`,
+    format: 'iife',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
     }
-};
+  },
+  plugins: [
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+    // We need to convert the CommonJS modules used by Node to ES6
+    commonjs(),
+    // Resolve Node modules
+    nodeResolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+  watch: {
+    exclude: ['node_modules/**']
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
-"use strict";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import React from "react";
-import ReactDOM from "react-dom";
-
-const app = document.getElementById("app");
+const app = document.getElementById('app')
 
 class Hello extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <h1>Hello, World!</h1>
-    }
+  render () {
+    return <h1>Hello, World!</h1>
+  }
 }
-
 
 ReactDOM.render(<Hello />, app)
